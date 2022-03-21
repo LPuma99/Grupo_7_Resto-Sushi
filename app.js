bf03199a0fb97967c977b00fc00f4bc2291dac44
LPuma99 <pumalucas8@gmail.com>
2022-03-21 20:38:11 -0300

    Header original, footer original

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app= express()
 const path = require('path')
 const PORT = 3000
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname + '/public')))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
 
