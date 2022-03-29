e5f0fb7b01c45ff9cda41ffe6d4483bdc27c4981
LPuma99 <pumalucas8@gmail.com>
2022-03-29 01:46:20 -0300

    detalle de producto

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const PORT = 3000
 
 app.use(express.static(path.join(__dirname + '/public')))
+/*  */
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
 
