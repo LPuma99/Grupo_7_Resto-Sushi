fd2dc7deb87bef950bdd0a8330072342bfc2b742
Rodrigo <98191682+Rodri-Ez@users.noreply.github.com>
2022-04-02 12:52:33 -0300

    Correcion del app.js y agregado de rutas

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-const express =require ('express')
-const app= express()
-const path = require('path')
+const express = require("express") 
+const app = express() 
+const path = require("path")
 const PORT = 3000
 
-app.use(express.static(path.join(__dirname + '/public')))
-/*  */
+app.use(express.static("public"))
+app.use("/public", express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
+
+app.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+
+})
+app.get("/login", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+
+})
+app.get("/registro", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/registro.html"))
+
+})
+app.get("/producto", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
+
+})
+app.get("/carrito", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/carrito-compra.html"))
+
+})
+
 
 app.listen(PORT, () => console.log(`Servidor listen port ${PORT}
 http://localhost:${PORT}`));
