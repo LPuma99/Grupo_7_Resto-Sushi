7acdc3efccd01843d9d25751b82059c3a623689d
LPuma99 <pumalucas8@gmail.com>
2022-04-08 20:00:21 -0300

    ruta

--- src/app.js
+++ src/app.js
@@ -10,12 +10,11 @@ const userRouter = require('./router/userRouter')
 
 /* View config */
 app.set('view engine', 'ejs')
-app.set('views', 'src/views')
+app.set('views', 'src/views/users')
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-/* app.use(express.static("public"))
-app.use("/style", express.static(__dirname + '../public/')) */
+
 
 app.use('/', indexRouter)
 app.use('/productos' , productRouter) /* Lista, detalle de producto */
